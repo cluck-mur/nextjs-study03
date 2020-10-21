6d66972554dff74eb8d6044ccf97dcbaff971e3c
cluck-mur <cluck.mur@ahk.jp>
2020-10-21 19:08:46 +0900

    ・ 20201021の作業分 その2 　　helloWorldGetStaticProps.tsx

--- pages/helloWorldGetStaticProps.tsx
+++ pages/helloWorldGetStaticProps.tsx
@@ -1,16 +1,27 @@
 import { GetStaticProps } from 'next'
-import { FunctionComponent } from 'react'
 
-const HelloWorld = ({ tag_string }) => {
-    return <div>{tag_string}</div>
+type MemberOfPropsForHelloWorldGetStatic = {
+    tag_string: string
+}
+type PropsForHelloWorldGetStaticProps = {
+    props: MemberOfPropsForHelloWorldGetStatic
+}
+
+const HelloWorldGetStaticProps = (object: MemberOfPropsForHelloWorldGetStatic) => {
+    return <div>{object.tag_string}</div>
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-    return {
-        props: {
-            tag_string: 'Hello World! with getStaticProps X Typescript'
-        }
+//export const getStaticProps: GetStaticProps = () => {     // これはエラーになる。 async型関数でないとダメ。
+    let props_member: MemberOfPropsForHelloWorldGetStatic = {
+        tag_string: `Hello World! (getStaticProps x TypeScript version)`
     }
+
+    let return_val: PropsForHelloWorldGetStaticProps = {
+        props: props_member
+    }
+
+    return return_val;
 }
 
-export default HelloWorld
+export default HelloWorldGetStaticProps
