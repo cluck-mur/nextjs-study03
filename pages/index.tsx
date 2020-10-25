f4f7a3afb9c696185ca1265ab12e86cec156f51e
cluck-mur <cluck.mur@ahk.jp>
2020-10-25 16:38:14 +0900

    ・VSCodeでデバッグできるように設定した。 ・POSTメッセージから body 部分を読み取るのに悪戦苦闘中

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-export default function Home() {
+const Home = ({ err }) => {
+  // const router = useRouter()
+
   return (
     <div>
       <Head>
@@ -22,7 +26,27 @@ export default function Home() {
         <div>
           <Link href="ssrStars"><a>SsrStars</a></Link>
         </div>
+        <div>
+          <form action="" method="post">
+            メモ: <input type="text" name="memo" size="40"></input><br />
+            <input type="submit" value="OK"></input>
+          </form>
+        </div>
       </main>
     </div>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  //console.log(context)
+  const trailers = context.req.trailers
+  console.log(trailers)
+
+  return {
+    props: {
+      err: 0
+    }
+  }
+}
+
+export default Home
